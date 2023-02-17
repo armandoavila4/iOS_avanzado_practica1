1d9596dfc4e943c7b4cb08ebe362e793a5b5cbaf
Armando <armando_avila_4>
2023-02-16 20:57:26 -0600

    Se agrega el package lottie

---
+++ Practica1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica1
+//
+//  Created by Jorge Armando Avila Estrada on 16/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
